d37ecc8d7f0ec311e0f584696a7566cc8889471c
Jephtebb <jephteborno@yahoo.com>
2021-04-04 16:16:13 -0400

    header navigation

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer() {
+
+
+    return (
+        <section>
+            
+        </section>
+
+    )
+
+}
+export default Footer
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <header>
+      <h2>
+        Jephte Borno
+      </h2>
+      <nav className="nav-bar">
+        <ul className="ulist">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+          <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+          <a href="#contact">Contact</a>
+          </li>
+          <li>
+          <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
